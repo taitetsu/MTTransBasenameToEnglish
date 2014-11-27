51499b0fce168ad780a3fd29e192b4fa1c376259
Taitetsu <tai-tetsu@hazama.nu>
2014-11-27 22:08:20 +0900

    Added L10N.

---
+++ plugins/MTTransBasenameToEnglish/lib/MTTransBasenameToEnglish/L10N/en_us.pm
@@ -0,0 +1,9 @@
+package MTTransBasenameToEnglish::L10N::en_us;
+
+use strict;
+use utf8;
+use base 'MTTransBasenameToEnglish::L10N';
+use vars qw( %Lexicon );
+%Lexicon = ();
+
+1;
---
+++ plugins/MTTransBasenameToEnglish/lib/MTTransBasenameToEnglish/L10N/ja.pm
@@ -0,0 +1,12 @@
+package MTTransBasenameToEnglish::L10N::ja;
+
+use strict;
+use utf8;
+use base 'MTTransBasenameToEnglish::L10N::en_us';
+use vars qw( %Lexicon );
+%Lexicon = (
+    'Taitetsu' => '太鉄',
+    'Entry title or inputted string is translated by Microsoft Translator, and set to basename.' => '記事タイトルあるいは入力された文字列を英語に翻訳し、ベースネームに挿入します',
+);
+
+1
